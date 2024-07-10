eb48f6d3fe8c04fe0a6dcc23694c1f0ffa090eb0
Naimuddin Syed <naimusyed72@gmail.com>
2024-07-10 18:22:39 +0530

    Delete credit directory

--- credit/src/main/java/credit/credit/repository/CreditCardRepository.java
+++
@@ -1,9 +0,0 @@
-package credit.credit.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import credit.credit.request.CreditCard;
-
-@Repository
-public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
-}
